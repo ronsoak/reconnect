4eb45f2079c78d56e256d80d8a407b6c8a3a5f97
CHRISTIAN YOUNG <CHRISTIAN.YOUNG@OSPRI.CO.NZ>
2024-06-14 18:25:18 +1200

    changed rss reader script to better account for issues

--- website/management/commands/read_rss.py
+++ website/management/commands/read_rss.py
@@ -2,37 +2,67 @@ from typing import Any
 from django.core.management.base import BaseCommand
 import feedparser
 import ssl
+import warnings
 from datetime import datetime
 from zoneinfo import ZoneInfo
 from website.models import Sites, Logic
 from django.core import management
 from bs4 import BeautifulSoup
+from bs4 import MarkupResemblesLocatorWarning
 # This script reads the RSS feeds of sites that have an RSS method listed against them.
 class Command(BaseCommand):
     def handle(self, *args: Any, **options: Any):
+        # Supress guessing parser warning for pure XML feeds
+        warnings.filterwarnings('ignore', category=MarkupResemblesLocatorWarning)
+        
         # Fixes Feedparser not getting an SSL cert
         ssl._create_default_https_context=ssl._create_unverified_context 
+        
+        # Gets the RSS identifier from the Logic Page
         rss_id = Logic.objects.get(category="METHOD", value = "RSS").id
+
+        # Gets the sites that are using the RSS method
         sources = Sites.objects.filter(feed_type=rss_id, hidden=False)
+
+        # Start to cycle through the sites
         for s in sources:
             url = s.feed
             name = s.name
             id = s.id 
             logo = s.logo.url
             error_check = False
-            self.stdout.write("Parsing Site:"+s.name) 
+            self.stdout.write("Reading Site: "+s.name) 
+
+            # Attempt to parse the URL
             try:
                 content = feedparser.parse(url)
             except:
                 error_check = True
                 self.stdout.write("failed to parse feed: " + name)
             while error_check == False:
-                if content.status >= 400:
+                # Try to ascertain the status of the feed
+                # If it fails, the script will still try to get content
+                try:
+                    feed_status = content.status
+                except:
+                    feed_status = 999
+
+                if feed_status >= 400:
                     error_check = True
-                    self.stdout.write("Bad HTTP Status: "+ name +"["+str(content.status)+"]")
-                if content.bozo == 1:
+                    self.stdout.write("Bad HTTP Status: "+ name +"["+str(feed_status)+"]")
+
+                # Try to ascertain the bozo status 
+                # If it fails, the script will still try to get content
+                try:
+                    bozo_status = content.bozo
+                except:
+                    bozo_status = 0
+
+                if bozo_status == 1:
                     error_check = True
-                    self.stdout.write("Bad Bozo Flag: "+ name +"["+str(content.bozo_exception)+"]")
+                    self.stdout.write("Bad Bozo Flag: "+ name +"["+str(bozo_status)+"]")
+
+                # Get the feed entries
                 for c in content.entries:
                     # Try and get the date
                     try:
@@ -60,14 +90,14 @@ class Command(BaseCommand):
                                 # Scan the content for the first Img reference
                                 try:
                                     item_content = str(c.content)
-                                    soup1 = BeautifulSoup(item_content, 'html.parser')
+                                    soup1 = BeautifulSoup(item_content, 'html5lib')
                                     image1 = soup1.find('img')
                                     post_image = image1['src']
                                 except:
                                     # Scan the description for the first Img reference
                                     try:
                                         item_desc = str(c.description)
-                                        soup2 = BeautifulSoup(item_desc, 'html.parser')
+                                        soup2 = BeautifulSoup(item_desc, 'html5lib')
                                         image2 = soup2.find('img')
                                         post_image = image2['src']
                                     except:
