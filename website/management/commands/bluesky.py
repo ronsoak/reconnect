87191378cbdac4893f03c562324191ed7dcc4af2
Alex Antra <12378656+ronsoak@users.noreply.github.com>
2025-03-16 23:02:42 +1300

    bsky fixes

--- website/management/commands/bluesky.py
+++ website/management/commands/bluesky.py
@@ -14,18 +14,20 @@ class Command(BaseCommand):
         bKey = 'password'
         # if results are zero exit 
         if bquery.count() == 0: 
+            self.stdout.write("No sites to post, ending script") 
             quit() 
         else:
             for b in bquery:
                 # start bluesky client  
                 client = Client()
                 # log in 
+                self.stdout.write("Logging in") 
                 client.login('antranaut.com',bKey)
                 # Get query details
                 aTitle = b.title
                 aLink = b.url
-                aSite = b.site.name
                 iURL = b.image_url
+                self.stdout.write("values:"+aTitle+' '+aLink+' '+iURL) 
 
                 # Fixes issue with having an SSL cert
                 ssl._create_default_https_context=ssl._create_unverified_context 
@@ -35,23 +37,27 @@ class Command(BaseCommand):
                     iPath = httpx.get(iURL).content
                     aThumb = client.upload_blob(iPath).blob
                 except:
-                    # Start Text Builder 
-                    text_builder = client_utils.TextBuilder()
-                    text_builder.text('Reconnect Recommends: \n\n')
-                    text_builder.text(aTitle+'\n\n')
-                    text_builder.tag('#gaming', 'gaming')
-                    text_builder.text(' ')
-                    text_builder.tag('#videogames', 'videogames')
-    
-                    # AppBskyEmbedExternal is the same as "link card" in the app
-                    embed_external = models.AppBskyEmbedExternal.Main(
-                        external=models.AppBskyEmbedExternal.External(
-                            title=aTitle,
-                            description='This article has been curated by Reconnect, the games writing discovery platform.',
-                            uri= aLink,
-                            thumb=aThumb,
-                        )
-                    )
-                    client.send_post(text=text_builder, embed=embed_external)
+                    self.stdout.write("Image fetch failed")
                     b.bluesky = True 
                     b.save()
+                    quit() 
+                # Start Text Builder 
+                text_builder = client_utils.TextBuilder()
+                text_builder.text('Reconnect Recommends: \n\n')
+                text_builder.text(aTitle+'\n\n')
+                text_builder.tag('#gaming', 'gaming')
+                text_builder.text(' ')
+                text_builder.tag('#videogames', 'videogames')
+
+                # AppBskyEmbedExternal is the same as "link card" in the app
+                embed_external = models.AppBskyEmbedExternal.Main(
+                    external=models.AppBskyEmbedExternal.External(
+                        title=aTitle,
+                        description='This article has been curated by Reconnect, the games writing discovery platform.',
+                        uri= aLink,
+                        thumb=aThumb,
+                    )
+                )
+                client.send_post(text=text_builder, embed=embed_external)
+                b.bluesky = True 
+                b.save()
