d1a5be711b1a4cf6b3559cbfd8f8a4bd201b4d59
Alex Antra <12378656+ronsoak@users.noreply.github.com>
2025-03-16 22:57:38 +1300

    fixed bug in curated script and changed bluesky script

--- website/management/commands/bluesky.py
+++ website/management/commands/bluesky.py
@@ -1,5 +1,4 @@
 # imports 
-import os
 from typing import Any
 from django.core.management.base import BaseCommand
 from atproto import Client, models, client_utils
@@ -11,8 +10,8 @@ import httpx
 class Command(BaseCommand):
     def handle(self, *args: Any, **options: Any):
         # query to get curated articles that havn't been posted already
-        bquery = Articles.objects.filter(hidden=False, site_hide=False,curated=True, bluesky=False).select_related("site").order_by('-published')[:1]
-        bKey = os.getenv("BLUESKY_KEY")
+        bquery = Articles.objects.filter(hidden=False, site_hide=False,curated=True, bluesky=False).select_related("site").order_by('published')[:1]
+        bKey = 'password'
         # if results are zero exit 
         if bquery.count() == 0: 
             quit() 
@@ -32,26 +31,27 @@ class Command(BaseCommand):
                 ssl._create_default_https_context=ssl._create_unverified_context 
 
                 # Get image thumbnail
-                iPath = httpx.get(iURL).content
-                aThumb = client.upload_blob(iPath).blob
-
-                # Start Text Builder 
-                text_builder = client_utils.TextBuilder()
-                text_builder.text('Reconnect Recommends: \n\n')
-                text_builder.text(aTitle+'\n\n')
-                text_builder.tag('#gaming', 'gaming')
-                text_builder.text(' ')
-                text_builder.tag('#videogames', 'videogames')
-
-                # AppBskyEmbedExternal is the same as "link card" in the app
-                embed_external = models.AppBskyEmbedExternal.Main(
-                    external=models.AppBskyEmbedExternal.External(
-                        title=aTitle,
-                        description='This article has been curated by Reconnect, the games writing discovery platform.',
-                        uri= aLink,
-                        thumb=aThumb,
+                try:
+                    iPath = httpx.get(iURL).content
+                    aThumb = client.upload_blob(iPath).blob
+                except:
+                    # Start Text Builder 
+                    text_builder = client_utils.TextBuilder()
+                    text_builder.text('Reconnect Recommends: \n\n')
+                    text_builder.text(aTitle+'\n\n')
+                    text_builder.tag('#gaming', 'gaming')
+                    text_builder.text(' ')
+                    text_builder.tag('#videogames', 'videogames')
+    
+                    # AppBskyEmbedExternal is the same as "link card" in the app
+                    embed_external = models.AppBskyEmbedExternal.Main(
+                        external=models.AppBskyEmbedExternal.External(
+                            title=aTitle,
+                            description='This article has been curated by Reconnect, the games writing discovery platform.',
+                            uri= aLink,
+                            thumb=aThumb,
+                        )
                     )
-                )
-                client.send_post(text=text_builder, embed=embed_external)
-                b.bluesky = True 
-                b.save()
+                    client.send_post(text=text_builder, embed=embed_external)
+                    b.bluesky = True 
+                    b.save()
--- website/management/commands/uncurate.py
+++ website/management/commands/uncurate.py
@@ -4,7 +4,7 @@ from website.models import Articles
 # This script un-curates articles to keep it at just 20, so the newer ones stay and the older ones are removed.
 class Command(BaseCommand):
     def handle(self, *args: Any, **options: Any):
-        a = Articles.objects.filter(curated=True,hidden=False,site_hide=False).order_by('-published')[21:99]
+        a = Articles.objects.filter(curated=True).order_by('-published')[21:99]
         for i in a:
             i.curated = False 
             i.save()
